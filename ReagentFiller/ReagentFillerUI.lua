5157b3f589cb9e891ec58d5f47f02e0eb98c8058
ChasingGoodGrades <chase.poland@gmail.com>
2026-01-17 01:07:40 -0500

    Restruct root folder

---
+++ ReagentFiller/ReagentFillerUI.lua
@@ -0,0 +1,379 @@
+--[[
+    
+ReagentFiller
+Copyright 2024 Aches (xarilawow@gmail.com)
+All right reserved.
+
+Creation of a Reagent Filler ReagentFiller that will fill the reagents for the player.
+
+]]
+local AceGUI = LibStub("AceGUI-3.0")
+local playersClass = select(2, UnitClass("player"))
+local DEFAULT_MAX_QUANTITY = 1000
+
+function ReagentFiller:CreateOptionsTable()
+    local classHandle = ""
+    for className, itemTypes in pairs(self.db.char.reagents) do
+        -- If classname to upper case is equal to the players class
+        if (className:upper() ~= playersClass) and self:CheckIfClassCategory(className)  then
+            classHandle = "|cff7f7f7f" .. className .. "|r"
+        else
+            classHandle = className
+        end
+        local categoryArgs = {
+            type = "group",
+            name = classHandle,
+            order = 1,
+            inline = false,
+            args = {},
+        }
+
+        if className == "--Custom--" then
+            self:AddCustomItems(categoryArgs, className, itemTypes)
+            self:AddCustomSubcategories(categoryArgs)
+        elseif className == "Hunter" then
+            self:CreateHunterOptions(categoryArgs, itemTypes)
+        else
+            for t_itemType, itemData in pairs(itemTypes) do
+                local subCategoryArgs = self:CreateSubCategoryOptions(t_itemType, itemData)
+                categoryArgs.args[t_itemType] = subCategoryArgs
+            end
+        end
+
+        self.options.args[className] = categoryArgs
+    end
+end
+
+function ReagentFiller:CreateSubCategoryOptions(t_itemType, t_itemData)
+    local subCategoryArgs = {
+        type = "group",
+        name = t_itemType,
+        order = 1,
+        inline = false,
+        args = {}
+    }
+
+    for itemID, itemData in pairs(t_itemData) do
+        local itemName = self:GetItemInfo(itemID) or "?"
+        local _, _, _, _, lvl = self:GetItemInfo(itemID)
+        local nameWithLevel = itemName .. (lvl and lvl ~= 0 and " (Level " .. lvl .. ")" or "")
+
+        if(itemData.version == nil or itemData.version <= WOW_PROJECT_ID) then
+            local args = {
+                type = "group",
+                name = nameWithLevel,
+                order = 1,
+                inline = true,
+                args = {}
+            }
+            args.args.icon = {
+                type = "description",
+                name = "",
+                image = function() return self:GetItemIcon(itemID) end,
+                order = 0,
+                width = "half",
+            }
+            args.args.enabled = {
+                type = "toggle",
+                name = "Enabled",
+                order = 1,
+                get = function(info) return itemData.enabled end,
+                set = function(info, value) 
+                    itemData.enabled = value 
+                end,
+            }
+            args.args.quantityToBuy = {
+                type = "input",
+                name = "Set Buy Limit",
+                desc = "Enter the quantity of " .. itemName .. " to buy up to / restock to. \n\n|cFFFF0000BEWARE: NO LIMIT|r",
+                order = 2,
+                width = "half",
+                get = function(info) return itemData.quantityToBuy ~= nil and tostring(itemData.quantityToBuy) or "0" end,
+                set = function(info, value) 
+                    itemData.quantityToBuy = tonumber(value) or 0
+                end,
+            }            
+            subCategoryArgs.args[itemName] = args
+        end
+    end
+
+    return subCategoryArgs
+end
+
+function ReagentFiller:AddCustomItems(categoryArgs, categoryName, categoryTable)
+    -- Define the "Create New Subcategory" section
+    local newSubCategoryName
+    categoryArgs.args.addSubcategory = {
+        type = "group",
+        name = "Create Your Own Subcategory",
+        inline = true,
+        order = 0,
+        args = {
+            subCategoryName = {
+                type = "input",
+                name = "Subcategory Name (e.g. Food)",
+                desc = "Enter the name of the new subcategory then press Enter.",
+                order = 1,
+                set = function(info, value)
+                    newSubCategoryName = value
+                    if categoryTable[newSubCategoryName] then
+                        self:Print("Subcategory already exists: " .. newSubCategoryName)
+                        return
+                    end
+                    if newSubCategoryName == "" then
+                        self:Print("Subcategory name cannot be empty.")
+                        return
+                    end
+                    self:Print("Added new subcategory named: " .. newSubCategoryName)
+                    self.db.char.reagents["--Custom--"][newSubCategoryName] = {}
+                    self:RefreshOptions()
+                end,
+            },
+        },
+    }
+
+    -- Define the "Edit Category" section
+    categoryArgs.args.editCategory = {
+        type = "group",
+        name = "Edit Category",
+        inline = true,
+        order = 1,
+        args = {
+            subCategoryDropdown = {
+                type = "select",
+                name = "Select Subcategory",
+                desc = "Select a subcategory to edit",
+                order = 0,
+                width = 1.25,
+                values = function()
+                    local dropdownValues = {}
+                    for subCategoryName, _ in pairs(categoryTable) do
+                        dropdownValues[subCategoryName] = subCategoryName
+                    end
+                    return dropdownValues
+                end,
+                set = function(info, value)
+                    info.handler.selectedSubcategory = value
+                end,
+                get = function(info)
+                    return info.handler.selectedSubcategory
+                end,
+            },
+            itemName = {
+                type = "input",
+                name = "Add an Item by ID or Item Link",
+                desc = "Enter the item's ID or shift click the item to link.",
+                order = 1,
+                width = 1.25,
+                set = function(info, value)
+                    local itemID = self:GetItemID(value)
+                    if(info.handler.selectedSubcategory == nil) then
+                        self:Print("Please select a subcategory first.")
+                        return
+                    end
+                    if not itemID then
+                        self:Print("Invalid item name, ID, or link.")
+                        return
+                    end
+                    if categoryTable[info.handler.selectedSubcategory][itemID] then
+                        self:Print("Item already exists: " .. itemID)
+                        return
+                    end
+                    self.db.char.reagents["--Custom--"][info.handler.selectedSubcategory][itemID] = {
+                        batchSize = 1,
+                        enabled = false,
+                        quantityToBuy = 0,
+                    }
+                    self:RefreshOptions()
+                end,
+            },
+            removeSubcategory = {
+                type = "execute",
+                name = "Remove Subcategory",
+                desc = "Remove the selected subcategory",
+                order = 2,
+                width = "normal",
+                func = function(info)
+                    local subCategoryName = info.handler.selectedSubcategory
+                    if subCategoryName then
+                        if categoryName == "--Custom--" then
+                            ReagentFiller.db.char.reagents["--Custom--"][subCategoryName] = nil
+                        else
+                            categoryTable[subCategoryName] = nil
+                        end
+                        info.handler.selectedSubcategory = nil
+                    end
+                    self:RefreshOptions()
+                end,
+            },
+        },
+    }
+end
+
+function ReagentFiller:AddCustomSubcategories(categoryArgs)
+    local customCategoryTable = self.db.char.reagents["--Custom--"]
+    if not customCategoryTable then
+        return
+    end
+
+    for subCategory, subCategoryTable in pairs(customCategoryTable) do
+        local subCategoryArgs = {
+            type = "group",
+            name = subCategory,
+            order = 2,
+            inline = false,
+            args = {}
+        }
+
+        for itemID, itemData in pairs(subCategoryTable) do
+            local itemName = self:GetItemInfo(itemID) or "?"
+            local _, _, _, _, lvl = self:GetItemInfo(itemID)
+            local nameWithLevel = itemName .. (lvl and lvl ~= 0 and " (Level " .. lvl .. ")" or "")
+
+            local args = {
+                type = "group",
+                name = nameWithLevel,
+                order = 1,
+                inline = true,
+                args = {}
+            }
+            args.args.icon = {
+                type = "description",
+                name = "",
+                image = function() return self:GetItemIcon(itemID) end,
+                order = 0,
+                width = "half",
+            }
+            args.args.enabled = {
+                type = "toggle",
+                name = "Enabled",
+                order = 1,
+                get = function(info) return itemData.enabled end,
+                set = function(info, value) 
+                    itemData.enabled = value 
+                end,
+            }
+            args.args.quantityToBuy = {
+                type = "input",
+                name = "Set Buy Limit",
+                desc = "Enter the quantity of " .. itemName .. " to buy up to / restock to. \n\n|cFFFF0000BEWARE: NO LIMIT|r",
+                order = 2,
+                width = "half",
+                get = function(info) return itemData.quantityToBuy ~= nil and tostring(itemData.quantityToBuy) or "0" end,
+                set = function(info, value) 
+                    itemData.quantityToBuy = tonumber(value) or 0
+                end,
+            }    
+            --[[
+            args.args.batchSize = {
+                type = "range",
+                name = "Size of Batch (vials are 5)",
+                order = 3,
+                min = 0,
+                max =  200,
+                step = 5,
+                get = function(info) return itemData.batchSize end,
+                set = function(info, value) 
+                    itemData.batchSize = value 
+                end,
+            }
+            ]]
+
+            subCategoryArgs.args[itemName] = args
+        end
+
+        categoryArgs.args[subCategory] = subCategoryArgs
+    end
+end
+
+function ReagentFiller:CreateHunterOptions(categoryArgs, categoryTable)
+    local subCategories = {}
+    local subCategoryArgs = {}
+
+    for subCategory, subCategoryTable in pairs(categoryTable) do
+        for itemID, itemData in pairs(subCategoryTable) do
+            local itemName = self:GetItemInfo(itemID) or "?"
+
+            if not subCategories[subCategory] then
+                local args = {
+                    type = "group",
+                    name = "Hunter " .. subCategory,
+                    order = 1,
+                    inline = true,
+                    args = {}
+                }
+                local fillName
+                if subCategory == "Bullets" then
+                    fillName = "Fill Pouch"
+                else
+                    fillName = "Fill Quiver"
+                end
+                args.args.fillQuiver = {
+                    type = "toggle",
+                    name = fillName,
+                    order = 1,
+                    get = function(info) return subCategoryTable.fillQuiver end,
+                    set = function(info, value) 
+                        subCategoryTable.fillQuiver = value
+                    end,
+                }
+
+                args.args.selectedItem = {
+                    type = "select",
+                    name = "Item",
+                    order = 2,
+                    width = 1.25,
+                    values = function()
+                        local itemValues = {}
+                        for id, data in pairs(subCategoryTable) do
+                            if((id ~= "fillQuiver" and id ~= "fillQuiverID") and (data.version == nil or data.version <= WOW_PROJECT_ID)) then
+                                itemValues[id] =  self:GetItemNameWithLevel(id)
+                            end
+                        end
+                        return itemValues
+                    end,
+                    get = function() return subCategoryTable.fillQuiverID end,
+                    set = function(info, value)
+                        subCategoryTable.fillQuiverID = value
+                    end,
+                    hidden = function() return not subCategoryTable.fillQuiver end
+                }
+
+                subCategoryArgs[subCategory] = args
+                subCategories[subCategory] = true
+            end
+
+            subCategoryArgs[subCategory].args[itemName] = args
+        end
+    end
+
+    for subCategory, args in pairs(subCategoryArgs) do
+        categoryArgs.args[subCategory] = args
+    end
+
+    return categoryArgs
+end
+
+function ReagentFiller:CreateLowReagentPopup()
+    -- Frame itself
+    local frame = AceGUI:Create("Frame")
+    frame:SetTitle("LOW REAGENT WARNING")
+    frame:SetWidth(200)
+    frame:SetHeight(60)
+    frame:SetPoint("CENTER", UIParent, "CENTER", 0, -100)
+    frame:SetStatusText("LESS THAN 50% OF SET REAGENTS")
+    frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
+    frame:SetLayout("List")
+
+    -- OK Button
+    local button = AceGUI:Create("Button")
+    button:SetText("OK")
+    button:SetWidth(170)
+    button:SetCallback("OnClick", function() AceGUI:Release(frame) end)
+    frame:AddChild(button)
+
+    frame:SetLayout("List")
+    frame:Show()
+end
+
+
---
+++ ReagentFiller/Utility.lua
@@ -0,0 +1,189 @@
+local CloseWindows = _G.CloseWindows
+
+--- WoW API Functions ---
+
+function ReagentFiller:GetItemInfo(itemID)
+    return GetItemInfo(itemID)       -- Deprecated but works in classic currently, expect to change.
+end
+
+function ReagentFiller:GetItemCount(itemID)
+    return GetItemCount(itemID)      -- Deprecated but works in classic currently, expect to change.
+end
+
+function ReagentFiller:GetItemIcon(itemID)
+    local itemIcon = GetItemIcon(itemID)    -- Deprecated but works in classic currently, expect to change.
+    return itemIcon
+end
+--- END API Functions ---
+
+function ReagentFiller:GetItemID(itemLink)
+    if itemLink then
+        local itemID = tonumber(itemLink:match("item:(%d+)"))
+        if itemID then
+            return itemID
+        end
+    end
+    if tonumber(itemLink) then
+        return tonumber(itemLink)
+    end
+    return nil
+end
+
+function ReagentFiller:GetItemNameWithLevel(itemID)
+    local itemName = self:GetItemInfo(itemID) or "?"
+    local _, _, _, _, lvl = self:GetItemInfo(itemID)
+    local nameWithLevel = itemName .. (lvl and lvl ~= 0 and " (Level " .. lvl .. ")" or "")
+
+    return nameWithLevel
+end
+
+function ReagentFiller:CheckIfClassCategory(className)
+    if className == nil then return end
+    if(className == "--Custom--") or (className == "-Miscellaneous-") then
+        return false
+    else
+        return true
+    end
+end
+function ReagentFiller:InAMajorCity()
+    local majorCities = {
+        -- Localization needed
+        ["Stormwind City"] = true,
+        ["Ironforge"] = true,
+        ["Darnassus"] = true,
+        ["Exodar"] = true,
+        ["Orgrimmar"] = true,
+        ["Thunder Bluff"] = true,
+        ["Undercity"] = true,
+        ["Silvermoon City"] = true,
+        ["Shattrath City"] = true,
+        ["Dalaran"] = true,
+    }
+    return majorCities[GetRealZoneText()]
+end
+
+function ReagentFiller:SetArrowCount()
+    if(WOW_PROJECT_WRATH_CLASSIC == WOW_PROJECT_ID) then
+        return 1000;
+    else
+        return 200;
+    end
+end
+
+function ReagentFiller:GetQuiverCapacityAndOccupiedSlotsFromBag()
+    local quiverCapacity = 0
+    local quiverFreeSlots = 0
+    local quiverArrowCount = 0
+    local tempQuiverArrowCount = 0
+    local tempItemIDArray = {}
+
+    for bag = 0, NUM_BAG_SLOTS do
+        local bagName = C_Container.GetBagName(bag)
+        if bagName then
+            local _, _, _, _, _, itemType = self:GetItemInfo(bagName)
+            if itemType == "Quiver" then
+                local numSlots = C_Container.GetContainerNumSlots(bag)
+                quiverCapacity = quiverCapacity + numSlots
+
+                for slot = 1, numSlots do
+                    local tempItemID = C_Container.GetContainerItemID(bag, slot) or 0
+                    tempQuiverArrowCount = self:GetItemCount(tempItemID)
+
+                    local container = C_Container.GetContainerItemInfo(bag, slot)
+                    if container == nil then
+                        quiverFreeSlots = quiverFreeSlots + 1
+                    end
+                    if((tempQuiverArrowCount > 0) and not tempItemIDArray[tempItemID]) then
+                        tempItemIDArray[tempItemID] = true
+                        quiverArrowCount = quiverArrowCount + tempQuiverArrowCount
+                    end
+                end
+            end
+        end
+    end
+
+    return quiverCapacity, quiverFreeSlots, quiverArrowCount
+end
+
+function ReagentFiller:GetSubsections()
+    local itemTypes = {}
+    for classType, itemType in pairs(self.db.char.reagents) do
+        for itemcategory, itemID in pairs(itemType) do
+            if type(itemID) == "table" then
+                table.insert(itemTypes, itemID)
+            end
+        end
+    end
+    return itemTypes
+end
+
+function ReagentFiller:GetItemData(itemTypes)
+    local items = {}
+    for itemID, itemData in pairs(itemTypes) do
+        if type(itemData) == "table" and itemData.enabled then
+            table.insert(items, {itemID = itemID, itemData = itemData})
+        end
+    end
+    return items
+end
+
+function ReagentFiller:CheckIfLowOnEnabledReagents()
+    local itemID, itemData, quantityToBuy, itemCount
+    local itemTypes = self:GetSubsections()
+    for _, t_itemID in ipairs(itemTypes) do
+        local items = self:GetItemData(t_itemID)
+        for _, itemInfo in ipairs(items) do
+            itemID = itemInfo.itemID
+            itemData = itemInfo.itemData
+            quantityToBuy = itemData.quantityToBuy or 0
+            itemCount = self:GetItemCount(itemID)
+            local threshold = quantityToBuy * 0.50  -- 50% threshold
+
+            if itemCount <= threshold then
+                self:Print("Less than 50% of enabled reagents.")
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+function ReagentFiller:RefreshOptions()
+    CloseWindows()
+    self:CreateOptionsTable()
+end
+
+-- This function ASSUMES you have the currency to buy the items... 
+-- May be odd for items that require items instead of copper currency ? Unsure if it'll be a problem.
+function ReagentFiller:BuyItemFromMerchant(itemID, quantity)
+    local itemName = self:GetItemInfo(itemID)
+    for i = 1, GetMerchantNumItems() do
+        local name = GetMerchantItemInfo(i)
+        if name and name == itemName then
+            BuyMerchantItem(i, quantity)
+            -- self:Print("Bought " .. quantity .. " " .. itemName) -- DEBUG
+            break
+        end
+    end
+end
+
+function ReagentFiller:BuyReagentsFromMerchant(reagents)
+    for className, itemTypes in pairs(reagents) do
+        if type(itemTypes) == "table" then
+            for itemID, itemData in pairs(itemTypes) do
+                if type(itemData) == "table" and itemData.enabled then
+                    local quantityToBuy = itemData.quantityToBuy or 0
+                    local maxStack = select(8, GetItemInfo(itemID)) or 1 
+                    local quantityToBuyAdjusted = math.max(0, quantityToBuy - self:GetItemCount(itemID))
+   
+                    while quantityToBuyAdjusted > 0 do
+                        local batchQuantity = math.min(quantityToBuyAdjusted, maxStack)
+                        self:BuyItemFromMerchant(itemID, batchQuantity)
+                        quantityToBuyAdjusted = quantityToBuyAdjusted - batchQuantity
+                    end
+                end
+            end
+        end
+    end
+end
